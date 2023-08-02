c2ea2b101784e41fcf4d25cc56e4d95ed6e4d9e3
hng011 <hansnaufal.3040@gmail.com>
2023-08-02 21:41:53 +0700

    reupload

---
+++ index.js
@@ -0,0 +1,19 @@
+const opnBtn = document.querySelector(".opn-btn-js");
+const opnCard = document.querySelector(".card");
+const journeyBtn = document.querySelector(".journeybtn-js")
+
+opnBtn.addEventListener("click", () => {
+    const text = "Close the Card";
+    if(opnBtn.innerText != text){
+        opnBtn.innerText = text;
+        opnCard.setAttribute("class","card card-js");
+    } else {
+        opnBtn.innerText = "Open the Card!";
+        opnCard.removeAttribute("class","card-js")
+        opnCard.setAttribute("class","card");
+    }
+});
+
+journeyBtn.addEventListener("click",() => {
+    console.log("journey btn clicked");
+});
