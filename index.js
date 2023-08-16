b89fe117572a2a4ad1448f40e0c7080d6e9807fb
hng011 <hansnaufal.3040@gmail.com>
2023-08-16 07:07:11 +0700

    fix design

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const opnBtn = document.querySelector(".opn-btn-js");
 const opnCard = document.querySelector(".card");
 const journeyBtn = document.querySelector(".journeybtn-js")
+const wishes = document.querySelector(".wishes");
 
 opnBtn.addEventListener("click", () => {
     const text = "Close the Card";
     if(opnBtn.innerText != text){
         opnBtn.innerText = text;
         opnCard.setAttribute("class","card card-js");
+        wishes.style = "display:contents;"
     } else {
         opnBtn.innerText = "Open the Card";
         opnCard.removeAttribute("class","card-js")
         opnCard.setAttribute("class","card");
+        wishes.style = "display:none;"
     }
 });
 
